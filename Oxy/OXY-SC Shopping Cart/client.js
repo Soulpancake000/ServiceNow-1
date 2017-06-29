6be5abb8f1328aa00f1a491ea89357944e0efaca
miguel <miguel@gammapartners.com>
2017-06-29 12:09:28 -0600

    estimated completion

--- Oxy/OXY-SC Shopping Cart/client.js
+++ Oxy/OXY-SC Shopping Cart/client.js
@@ -1,125 +1,127 @@
 function ($scope, spUtil, $location, $timeout) {
-  var c = this;
-  c.m = $scope.data.msgs;
-  c.deliverTo = "";
-  c.additionalDetails = "";
-  c.options.cartTemplate = c.options.cartTemplate || 'oxy_large_shopping_cart.html';
-	
-  $scope.$on("$sp.service_catalog.cart.update", function() {
-    $scope.server.update().then(function() {
-      $scope.$emit("$sp.service_catalog.cart.count", getItemCount());
-    });
-  });
+    var c = this;
+    c.m = $scope.data.msgs;
+    c.deliverTo = "";
+    c.additionalDetails = "";
+    c.options.cartTemplate = c.options.cartTemplate || 'oxy_large_shopping_cart.html';
+    c.data.completion_date = c.data.completion_date.value ? moment().add(c.data.completion_date.value, c.data.completion_date.time).format('LL') : 'Uknown';
 
-  c.updateQuantity = function(item) {
-    c.server.get({
-      action: "update_quantity",
-      itemID: item.sys_id,
-      quantity: item.quantity
-    }).then(function(response) {
-      $rootScope.$broadcast("$sp.service_catalog.cart.update");
+    $scope.$on("$sp.service_catalog.cart.update", function() {
+        $scope.server.update().then(function() {
+            $scope.$emit("$sp.service_catalog.cart.count", getItemCount());
+            c.data.completion_date = c.data.completion_date.value ? moment().add(c.data.completion_date.value, c.data.completion_date.time).format('LL') : 'Uknown';
+        });
     });
-  }
 
-  c.triggerCheckout = function($evt, twostepCheckout) {
-    if (twostepCheckout) {
-      $location.url('?id=oxy_sc_cart');
-      return;
+    c.updateQuantity = function(item) {
+        c.server.get({
+            action: "update_quantity",
+            itemID: item.sys_id,
+            quantity: item.quantity
+        }).then(function(response) {
+            $rootScope.$broadcast("$sp.service_catalog.cart.update");
+        });
     }
 
-    $evt.stopPropagation();
-    $evt.preventDefault();
+    c.triggerCheckout = function($evt, twostepCheckout) {
+        if (twostepCheckout) {
+            $location.url('?id=oxy_sc_cart');
+            return;
+        }
 
-    c.checkoutInProgress = true;
-    c.data.action = "checkout";
-    c.data.deliverTo = c.deliverTo;
-    c.data.additionalDetails = c.additionalDetails;
-    c.server.update().then(function(response) {
-      c.data.action = null;
-			c.redirecting = true;
-      c.checkoutInProgress = false;
-      
-			var requestData = $scope.data.requestData;
-      
-			$location.path('/it').search({
-				id: 'oxy_confirmation', 
-				sys_id:requestData.sys_id
-			});
-			
-			$rootScope.$broadcast("$sp.service_catalog.cart.update");
-    });
-  }
+        $evt.stopPropagation();
+        $evt.preventDefault();
 
-  c.removeItem = function($evt, item) {
-    $evt.stopPropagation();
-    $evt.preventDefault();
+        c.checkoutInProgress = true;
+        c.data.action = "checkout";
+        c.data.deliverTo = c.deliverTo;
+        c.data.additionalDetails = c.additionalDetails;
+        c.server.update().then(function(response) {
+            c.data.action = null;
+            c.redirecting = true;
+            c.checkoutInProgress = false;
 
-    $scope.server.get({
-      cartName: "DEFAULT",
-      action: "remove_item",
-      removeItemID: item.sys_id
-    }).then(function(response) {
-      c.data.cart = response.data.cart;
-      c.data.cartItems = response.data.cartItems
-      $rootScope.$broadcast("$sp.service_catalog.cart.update");
-    });
-  }
+            var requestData = $scope.data.requestData;
 
-  c.editItem = function(itemID) {
-    c.server.get({
-      itemID: itemID,
-      action: "edit_item"
-    }).then(function(response) {
-      var myModalCtrl;
-      var unregister = $scope.$on('$sp.service_catalog.cart.update', function(){
-        myModalCtrl.close();
-      });
+            $location.path('/it').search({
+                id: 'oxy_confirmation',
+                sys_id:requestData.sys_id
+            });
 
-      var myModal = angular.copy(response.data.editVariablesModal);
-      myModal.options.afterOpen = function(ctrl){
-        myModalCtrl = ctrl;
-      };
-      myModal.options.afterClose = function() {
-        unregister();
-        c.editVariablesModal = null;
-        myModalCtrl = null;
-      };
-      c.editVariablesModal = myModal;
-    })
-  }
+            $rootScope.$broadcast("$sp.service_catalog.cart.update");
+        });
+    }
 
-  c.saveBundle = function() {
-    var saveCartModalCtrl;
-    var unregister = $scope.$on('$sp.service_catalog.cart.save_cart', function(){
-      saveCartModalCtrl.close();
-    });
+    c.removeItem = function($evt, item) {
+        $evt.stopPropagation();
+        $evt.preventDefault();
 
-    var saveCartModal = angular.copy(c.data.saveCartModal);
-    saveCartModal.options.afterOpen = function(ctrl){
-      saveCartModalCtrl = ctrl;
+        $scope.server.get({
+            cartName: "DEFAULT",
+            action: "remove_item",
+            removeItemID: item.sys_id
+        }).then(function(response) {
+            c.data.cart = response.data.cart;
+            c.data.cartItems = response.data.cartItems
+            $rootScope.$broadcast("$sp.service_catalog.cart.update");
+        });
+    }
+
+    c.editItem = function(itemID) {
+        c.server.get({
+            itemID: itemID,
+            action: "edit_item"
+        }).then(function(response) {
+            var myModalCtrl;
+            var unregister = $scope.$on('$sp.service_catalog.cart.update', function(){
+                myModalCtrl.close();
+            });
+
+            var myModal = angular.copy(response.data.editVariablesModal);
+            myModal.options.afterOpen = function(ctrl){
+                myModalCtrl = ctrl;
+            };
+            myModal.options.afterClose = function() {
+                unregister();
+                c.editVariablesModal = null;
+                myModalCtrl = null;
+            };
+            c.editVariablesModal = myModal;
+        })
+    }
+
+    c.saveBundle = function() {
+        var saveCartModalCtrl;
+        var unregister = $scope.$on('$sp.service_catalog.cart.save_cart', function(){
+            saveCartModalCtrl.close();
+        });
+
+        var saveCartModal = angular.copy(c.data.saveCartModal);
+        saveCartModal.options.afterOpen = function(ctrl){
+            saveCartModalCtrl = ctrl;
+        };
+        saveCartModal.options.afterClose = function() {
+            unregister();
+            c.saveCartModal = null;
+            saveCartModalCtrl = null;
+        };
+        c.saveCartModal = saveCartModal;
+    }
+
+    c.requestedFor = {
+        displayValue: c.data.cart.requested_for_display_name,
+        value: c.data.cart.requested_for,
+        name: 'requested_for'
     };
-    saveCartModal.options.afterClose = function() {
-      unregister();
-      c.saveCartModal = null;
-      saveCartModalCtrl = null;
-    };
-    c.saveCartModal = saveCartModal;
-  }
 
-  c.requestedFor = {
-    displayValue: c.data.cart.requested_for_display_name,
-    value: c.data.cart.requested_for,
-    name: 'requested_for'
-  };
+    $scope.$on("field.change", function(evt, parms) {
+        if (parms.field.name == 'requested_for')
+            c.data.cart.requested_for = parms.newValue;
+    });
 
-  $scope.$on("field.change", function(evt, parms) {
-    if (parms.field.name == 'requested_for')
-      c.data.cart.requested_for = parms.newValue;
-  });
+    $scope.$emit("$sp.service_catalog.cart.count", getItemCount());
 
-  $scope.$emit("$sp.service_catalog.cart.count", getItemCount());
-
-  function getItemCount() {
-    return c.data.cartItems.length || 0;
-  }
+    function getItemCount() {
+        return c.data.cartItems.length || 0;
+    }
 }
--- Oxy/OXY-SC Shopping Cart/server.js
+++ Oxy/OXY-SC Shopping Cart/server.js
@@ -1,62 +1,86 @@
 (function() {
-	var m = data.msgs = {};
-	m.createdMsg = gs.getMessage("Created");
-	m.trackMsg = gs.getMessage("track using 'Requests' in the header or");
-	m.clickMsg = gs.getMessage("click here to view");
-		
-	var userID = gs.getUser().getID();
-	var cart = new SPCart(input.cartName, userID);
+    var m = data.msgs = {};
+    m.createdMsg = gs.getMessage("Created");
+    m.trackMsg = gs.getMessage("track using 'Requests' in the header or");
+    m.clickMsg = gs.getMessage("click here to view");
 
-	if (input && input.action === "edit_item") {
-		data.editVariablesModal = $sp.getWidget('widget-modal', {embeddedWidgetId: 'sp-variable-editor', embeddedWidgetOptions: {sys_id: input.itemID, table: "sc_cart_item", isOrdering: true}});
-		return;
-	}
+    var userID = gs.getUser().getID();
+    var cart = new SPCart(input.cartName, userID);
 
-	if (input && input.action === "update_quantity") {
-		var cartItemGR = new GlideRecord("sc_cart_item");
-		if (cartItemGR.get(input.itemID)) {
-			cartItemGR.setValue("quantity", input.quantity);
-			cartItemGR.update();
-		}
-	}
+    if (input && input.action === "edit_item") {
+        data.editVariablesModal = $sp.getWidget('widget-modal', {embeddedWidgetId: 'sp-variable-editor', embeddedWidgetOptions: {sys_id: input.itemID, table: "sc_cart_item", isOrdering: true}});
+        return;
+    }
 
-	if (input && input.action === "checkout") {
-		cart.setSpecialInstructions(input.additionalDetails);
-		cart.setRequestedFor(input.cart.requested_for);
-		cart.setDeliveryAddress(input.deliverTo);
-		var request = cart.placeOrder();
-		data.requestData = {
-			number: request.getValue("number"),
-			table: request.getTableName(),
-			sys_id: request.getUniqueValue()
-		}
-		cart.setSpecialInstructions("");
-		cart.setRequestedFor(userID);
-		cart.setDeliveryAddress("");
-	}
+    if (input && input.action === "update_quantity") {
+        var cartItemGR = new GlideRecord("sc_cart_item");
+        if (cartItemGR.get(input.itemID)) {
+            cartItemGR.setValue("quantity", input.quantity);
+            cartItemGR.update();
+        }
+    }
 
-	if (input && input.action === "remove_item") {
-		var itemGR = new GlideRecord('sc_cart_item');
-		if (itemGR.get(input.removeItemID))
-			itemGR.deleteRecord();
-	}
+    if (input && input.action === "checkout") {
+        cart.setSpecialInstructions(input.additionalDetails);
+        cart.setRequestedFor(input.cart.requested_for);
+        cart.setDeliveryAddress(input.deliverTo);
+        var request = cart.placeOrder();
+        data.requestData = {
+            number: request.getValue("number"),
+            table: request.getTableName(),
+            sys_id: request.getUniqueValue()
+        }
+        cart.setSpecialInstructions("");
+        cart.setRequestedFor(userID);
+        cart.setDeliveryAddress("");
+    }
 
-	if (input && input.action === "save_cart") {
-		var newCart = new SPCart(input.newCartName, userID);
-		newCart.loadCart(cart.getCartID());
-	}
+    if (input && input.action === "remove_item") {
+        var itemGR = new GlideRecord('sc_cart_item');
+        if (itemGR.get(input.removeItemID))
+            itemGR.deleteRecord();
+    }
 
-	data.sys_properties = {
-		twostep_checkout: gs.getProperty("glide.sc.checkout.twostep", "false") == 'true'
-	};
+    if (input && input.action === "save_cart") {
+        var newCart = new SPCart(input.newCartName, userID);
+        newCart.loadCart(cart.getCartID());
+    }
 
-	var cartID = cart.getCartID();
-	data.saveCartModal = $sp.getWidget('widget-modal', {embeddedWidgetId: 'sc_save_bundle', embeddedWidgetOptions: {}});
+    data.sys_properties = {
+        twostep_checkout: gs.getProperty("glide.sc.checkout.twostep", "false") == 'true'
+    };
 
-	data.cart = cart.getCartObj();
-	data.cartItems = cart.getItems();
-	
-	data.completion_time = new Date(2017, 12, 31);
+    var cartID = cart.getCartID();
+    data.saveCartModal = $sp.getWidget('widget-modal', {embeddedWidgetId: 'sc_save_bundle', embeddedWidgetOptions: {}});
 
+    data.cart = cart.getCartObj();
+    data.cartItems = cart.getItems();
+
+	/* Estimate Completion Date*/
+    var estimateCompletionDate = function(){
+        var catItemIds = data.cartItems.map(function(item){ return item.item_id; });
+        var itemCatGR = new GlideRecord('sc_cat_item');
+        itemCatGR.addQuery('sys_id', 'IN', catItemIds);
+        itemCatGR.query();
+        itemCatGR.next();
+        var completion_date = itemCatGR.getValue('u_sla_duration');
+        while(itemCatGR.next()){
+            var u_sla_duration = itemCatGR.getValue('u_sla_duration');
+            if(u_sla_duration){
+                if(parseInt(completion_date) > parseInt(u_sla_duration)){
+                } else {
+                    completion_date = u_sla_duration;
+                }
+            }
+        }
+        var CompletionDate = {value: '', time: ''};
+        if(completion_date){
+            completion_date = completion_date.split(' ');
+            CompletionDate.value = completion_date[0];
+            CompletionDate.time = completion_date[1];
+        }
+        return CompletionDate;
+    };
+    data.completion_date = estimateCompletionDate();
 })();
 
