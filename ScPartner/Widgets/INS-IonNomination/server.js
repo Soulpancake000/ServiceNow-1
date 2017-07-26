8727632dc7559b3b89ad9d050ce5d013629196a1
miguel <miguel@gammapartners.com>
2017-07-26 12:06:34 -0600

    ScPartner - Widget - INS-IonNomination: Fetch requests

--- ScPartner/Widgets/INS-IonNomination/server.js
+++ ScPartner/Widgets/INS-IonNomination/server.js
@@ -1,5 +1,64 @@
-(function() {
+(function () {
     /* populate the 'data' object */
     /* e.g., data.table = $sp.getValue('table'); */
+    var opportunitiesTable = 'u_incidents',
+        requestTable = 'u_ion_nomination',
+        Pagination = {
+            items_in_pages: 10,
+            current_page: 1,
+            max_size: 10,
+            total_items: 0
+        },
+        sortDirection = 'Desc',
+        projectAttributes = [
+            'assigned_to',
+            'company',
+            'description',
+            'u_incidents',
+            'number',
+            'sys_id',
+            'state'
+        ];
+
+
+    //fetch requests
+    var gr = new GlideRecord(requestTable);
+    var requests = fetchItems(gr);
+
+    //Assign values to `data`
+    data.requests = requests;
+    console.log(requests);
+
+    function fetchItems(gr) {
+        gr = setPagination(gr);
+        var items = [];
+        //Fetch
+        gr.query();
+        while (gr.next()) {
+            var grRecord = $sp.getFieldsObject(gr, projectAttributes.join(','));
+            items.push(grRecord);
+        }
+        return items;
+    }
+
+    function serviceDispatch() {
+        if (input) {
+            if (input.action === 'get') {
+                FilterByState = input.filter_state;
+                OrderBy = input.order_by;
+                sortDirection = input.sort_direction;
+                Pagination = input.pagination;
+            }
+        }
+    }
+
+    function setPagination(gr) {
+        gr.query();
+        Pagination.total_items = gr.getRowCount();
+        var start = (Pagination.current_page - 1) * Pagination.items_in_pages,
+            end = (Pagination.current_page) * Pagination.items_in_pages;
+        gr.chooseWindow(start, end);
+        return gr;
+    }
 
 })();
