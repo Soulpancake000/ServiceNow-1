6acf75a629aa8ce6db4fac957db7e2082633c763
miguel <miguel@gammapartners.com>
2017-07-31 11:52:46 -0600

    ScPartner - Align Opportunities - Business Rule to populate incidents

--- ScPartner/CMS/IonNomination/SystemDefinition/BusinessRule/DevInstance/uIonNomination_Populate m2m incidents.js
+++ ScPartner/CMS/IonNomination/SystemDefinition/BusinessRule/DevInstance/uIonNomination_Populate m2m incidents.js
@@ -1,5 +1,31 @@
+/**
+ * @filterCondition: on Incidents Changes
+ * @whenToRun: After Insert, Update, Delete
+ * @order: 100
+ */
 (function executeRule(current, previous /*null when async*/) {
+    deletePreviousIncidents();
+    saveIncidents();
 
-    // Add your code here
+    function saveIncidents() {
+        var incidents = current.u_incidents.split(',');
+        incidents.forEach(function (sysIdInc) {
+            var glideIncNom = new GlideRecord('u_m2m_ion_nominations_incidents');
+            glideIncNom.initialize();
+            glideIncNom.u_incident = sysIdInc;
+            glideIncNom.u_ion_nomination = current.sys_id;
+            glideIncNom.update();
+        });
+    }
 
+    /**
+     * It will remove only the incidents in the referenced field in Nomination Table.
+     * Meaning that, if an incident was added outside the List reference field (in Incident Form)
+     * or the Glide List (in the record producer) it won't be deleted.
+     */
+    function deletePreviousIncidents() {
+        var glideIncNom = new GlideRecord('u_m2m_ion_nominations_incidents');
+        glideIncNom.addEncodedQuery('u_ion_nomination=' + current.sys_id + '^' + 'u_incidentIN' + previous.u_incidents);
+        glideIncNom.deleteMultiple();
+    }
 })(current, previous);
